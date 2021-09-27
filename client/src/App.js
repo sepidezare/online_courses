8089cd2058299698bb2795786a7f034679226be3
sepidezare <zare.sepide@gmail.com>
2021-09-27 18:07:05 +0330

    commit18

---
+++ client/src/App.js
@@ -0,0 +1,67 @@
+
+import './App.css';
+import data from './data';
+
+
+function App() {
+  return (
+    <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/" >COURSES</a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+<div className="row center">
+  {data.products.map(product=>(
+
+<div key={product._id} className="card">
+<a href={`/products/${product._id}`}>
+<img className="medium" src={product.image} alt={product.name} ></img>
+</a>
+
+<div className="card-body">
+<a href={`/products/${product._id}`}>
+   <h2>{product.name}</h2>
+  </a>
+  <div className="rating">
+    <span> <i className="fas fa-star"></i> </span>
+    <span> <i className="fas fa-star"></i> </span>
+    <span> <i className="fas fa-star"></i> </span>
+    <span> <i className="fas fa-star"></i> </span>
+    <span> <i className="fas fa-star"></i> </span>
+   
+  </div>
+ <div className="price"> ${product.price} </div>
+  </div>
+  </div> 
+
+
+
+  ))
+
+
+
+  }
+  
+
+ 
+
+  </div>          
+
+
+        </main>
+        
+        <footer className="row center">All rights reserved</footer>
+       
+      </div>    
+
+
+  );
+}
+
+export default App;
---
+++ client/src/data.js
@@ -0,0 +1,69 @@
+const data={
+products:[
+{
+_id:'1',
+name:'c# course',
+category:'computer courses',
+image:'/images/1.jpg',
+price: 100,
+rating: 4.5,
+numReviews: 10,
+description:'a full course of c# programming'
+},
+
+{
+    _id:'2',
+    name:'python course',
+    category:'computer courses',
+    image:'/images/2.jpg',
+    price: 120,
+    rating: 4,
+    numReviews: 10,
+    description:'a full course of pythhon programming'
+},
+{
+    _id:'3',
+    name:'php course',
+    category:'computer courses',
+    image:'/images/3.jpg',
+    price: 100,
+    rating: 4.5,
+    numReviews: 10,
+    description:'a full course of php programming'
+},
+{
+    _id:'4',
+    name:'react course',
+    category:'computer courses',
+    image:'/images/4.jpg',
+    price: 100,
+    rating: 4.5,
+    numReviews: 10,
+    description:'a full course of react programming'
+},
+{
+    _id:'5',
+    name:'node course',
+    category:'computer courses',
+    image:'/images/5.jpg',
+    price: 100,
+    rating: 4.5,
+    numReviews: 10,
+    description:'a full course of node programming'
+},
+{
+    _id:'6',
+    name:'java course',
+    category:'computer courses',
+    image:'/images/2.jpg',
+    price: 100,
+    rating: 4.5,
+    numReviews: 10,
+    description:'a full course of java programming'
+},
+
+],
+
+};
+
+export default data;
